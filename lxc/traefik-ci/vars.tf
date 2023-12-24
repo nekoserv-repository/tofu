ca15dc7378691a0940153c6c56aab0206c4df6ca
nekoserv-repository <44324593+nekoserv-repository@users.noreply.github.com>
2023-12-24 19:31:02 +0100

    Added : traefik-ci

---
+++ lxc/traefik-ci/vars.tf
@@ -0,0 +1,48 @@
+variable "hostname" {
+  default = "traefik-ci"
+}
+variable "cores" {
+   default = "1"
+}
+variable "memory" {
+   default = "128"
+}
+variable "disk" {
+   default = "512M"
+}
+variable "storage" {
+   default = "local-lvm"
+}
+variable "ssh_key" {
+   default = "none"
+}
+variable "proxmox_host" {
+    default = "proxmox"
+}
+variable "template_name" {
+    default = "local:vztmpl/alpine-3.19.0.tar.zst"
+}
+variable "ethernet_name" {
+    default = "eth0"
+}
+variable "bridge_name" {
+    default = "vmbr0"
+}
+variable "vlan_num" {
+    default = "-1"
+}
+variable "api_url" {
+   default = "none"
+}
+variable "token_secret" {
+   default = "none"
+}
+variable "token_id" {
+   default = "none"
+}
+variable "ip_addr" {
+   default = "none"
+}
+variable "gw" {
+   default = "none"
+}
