2c84235ffece0f68d8b9e01f40f4344b83cfa8ba
nekoserv-repository <44324593+nekoserv-repository@users.noreply.github.com>
2023-12-31 10:32:59 +0100

    Added : alpine vm

---
+++ vm/alpine/.terraform.lock.hcl
@@ -0,0 +1,24 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/bpg/proxmox" {
+  version     = "0.42.0"
+  constraints = "0.42.0"
+  hashes = [
+    "h1:Y9gbYb9H3YXUtycj58vXDbjCI4bg5izOxAmMeyn8OfM=",
+    "zh:09804e177d2d36d3904843152679592dc19d4d54c98d85570eb8ebaa67f8d56a",
+    "zh:0ca1c27a5e3b2d151f856e3199f33e3c7d08de886aed7e1b7f143c7c990df6f6",
+    "zh:16a6139187b989d44fd9c14ed8f623d58100caed0ca6238ea58327ba6d4c2b8e",
+    "zh:24e8063d0336fe2b2f4a09c1231b1cae783a210d18029d1851f8f9126b18641f",
+    "zh:462bac0ea6325ae223c81336a9f5ec7cc036cf12c3df3ad2db1b45bc0c780cc7",
+    "zh:47cec38b957b42def5843553993b4dfacc8e2dc98c57a211ff2d3f0b0a5eb3d2",
+    "zh:77e420d76e667a199e57725426e65a0adfd35fbd9a6f09d5ed157d8e3681f629",
+    "zh:820218c62a3030008e10b4ec068046c85efa27e6471c7af58b03926e9c0b7659",
+    "zh:8974e9789f67afffb6fd167d1941662025451c1d6183f00d60d1d6c7ae863028",
+    "zh:979bee1aa867c1ec23d33f15c366ecc2639c08a8eff021dfe37eb61e2c35e4af",
+    "zh:b30b0eafb0055a8f8fc0acb4cbec9daccb5055855e58cbac804234a148158250",
+    "zh:ca2f95e881528ae898722aa4349f5fabe084f08ab8475b30205cb8d8524fa865",
+    "zh:db0ed6991a55c88b078740603b6648d52b212ebb61c5f9479090d90e76138eba",
+    "zh:fd4628e0dab83b7032d43bcf7b995033ba0f92cdbafdd6073f29b35068aeeabb",
+  ]
+}
---
+++ vm/alpine/main.tf
@@ -0,0 +1,147 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.42.0"
+    }
+  }
+}
+
+
+provider "proxmox" {
+  endpoint = "https://${var.proxmox_ve}:8006/api2/json"
+  username = var.api_user
+  password = var.api_pass
+  insecure = true
+  tmp_dir  = "/var/tmp"
+  ssh {
+    agent = true
+    node {
+      name    = "proxmox"
+      address = var.proxmox_ve
+    }
+  }
+}
+
+
+resource "proxmox_virtual_environment_file" "cloud_image" {
+  count        = 1
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = var.proxmox_host
+
+  source_file {
+    path      = "nocloud_alpine-3.19.0-x86_64-bios-cloudinit-r0.qcow2"
+    file_name = "nocloud_alpine-3.19.0-x86_64-bios-cloudinit-r0.img"
+  }
+}
+
+
+resource "proxmox_virtual_environment_vm" "alpine_vm" {
+  count         = 1
+  name          = "alpine-vm"
+  node_name     = var.proxmox_host
+  vm_id         = count.index + 150
+
+  agent {
+    enabled = false
+  }
+
+  startup {
+    order      = "1"
+    up_delay   = "30"
+    down_delay = "30"
+  }
+
+  cpu {
+    cores = 3
+  }
+
+  memory {
+    dedicated = 1024
+  }
+
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    file_id      = proxmox_virtual_environment_file.cloud_image[count.index].id
+    interface    = "scsi0"
+    size         = 4
+  }
+
+  # cloud image expects a serial port to be present
+  serial_device {}
+
+  operating_system {
+    type = "l26" # 6.x - 2.6 Kernel
+  }
+
+  initialization {
+    #dns {
+    #  servers   = var.dns
+    #}
+    ip_config {
+      ipv4 {
+        address = var.ip_addr
+        gateway = var.gw
+      }
+    }
+    user_account {
+      username  = "alpine"
+      keys      = [ var.ssh_key ]
+    }
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config[count.index].id
+  }
+}
+
+
+resource "proxmox_virtual_environment_file" "cloud_config" {
+  count        = 1
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = var.proxmox_host
+  #node_name    = "server-0${count.index + 1}"
+
+  source_raw {
+    data = <<EOF
+#cloud-config
+#hostname: k8s-node-${count.index + 1}
+hostname: alpine-vm
+
+manage_resolv_conf: true
+resolv_conf:
+  nameservers:
+    - "${var.dns1}"
+    - "${var.dns2}"
+
+users:
+  - default
+  - name: alpine
+    shell: /bin/ash
+    ssh-authorized-keys:
+      - ${trimspace(var.ssh_key)}
+    sudo: ALL=(ALL) NOPASSWD:ALL
+
+write_files:
+  - path: /etc/sudoers.d/cloud-init
+    content: |
+      Defaults !requiretty
+
+package_update: true
+package_upgrade: true
+packages:
+  - qemu-guest-agent
+
+runcmd:
+  - rc-update add qemu-guest-agent default
+  - rc-service qemu-guest-agent start
+
+EOF
+    # Prevent files overwriting eachother by giving them unique names with the count.index
+    file_name = "alpine-terraform-cloud-config-${count.index}.yaml"
+  }
+}
